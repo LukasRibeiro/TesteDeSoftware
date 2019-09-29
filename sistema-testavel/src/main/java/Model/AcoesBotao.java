5dde264d4f48d4e3e72f29876c8c55740c579706
Lucas Ribeiro <luckas.ribeiro2015@gmail.com>
2019-09-29 13:47:15 -0300

    sistema testavel terminar

---
+++ sistema-testavel/src/main/java/Model/AcoesBotao.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.JTextField;
+
+// import model.AnalisadorSequencia;
+
+public class AcoesBotao {
+    
+    // AnalisadorSequencia chamaOMetodoPara = new AnalisadorSequencia();
+
+
+    // public void limparCampos(JTextField entrada, JTextField saida){
+
+    //     entrada.setText("");
+    //     saida.setText("");
+    // }
+        
+    // public Boolean testarCampos(JTextField entrada){
+
+    //     return chamaOMetodoPara.analisar(entrada.getText());
+    // }
+}
---
+++ sistema-testavel/src/main/java/Model/Janela.java
@@ -0,0 +1,87 @@
+package Model;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+// import model.AnalisadorSequencia;
+import Model.AcoesBotao;
+
+public class Janela extends JFrame{
+
+    AcoesBotao act = new AcoesBotao();
+
+    //metodo para abrir a janela
+    public void criarJanela(){
+
+        Container jal = getContentPane();
+        setLayout(null);
+  
+        AcoesBotao act = new AcoesBotao();
+
+        //input para digitar entrada
+        JLabel valor = new JLabel("Entrada: ");
+        JTextField SetValor = new JTextField(10);
+        
+        //input para mostrar saida
+        JLabel resultado = new JLabel("Saida: ");
+        JTextField SetSaida = new JTextField(10);
+        
+        //botoes da janela
+        JButton botaoTestar = new JButton("Testar");
+        JButton botaoLimpar = new JButton("Limpar campos");
+        
+        
+//=====================================================================================
+
+        //posicionamento dos campos de entrada e saida da janela
+        valor.setBounds(50,40,100,20);
+        resultado.setBounds(200,40,100,20);
+
+        SetValor.setBounds(50,80,100,20);
+        SetSaida.setBounds(200,80,100,20);
+        
+        //posicionamento botao
+        botaoTestar.setBounds(50,120,250,20);
+        botaoLimpar.setBounds(50,160,250,20);
+
+//=====================================================================================
+
+
+        //adicionando as propriedades nas janelas
+        jal.add(valor);
+        jal.add(SetValor);
+        jal.add(resultado);
+        jal.add(SetSaida);
+        jal.add(botaoTestar);
+        jal.add(botaoLimpar);
+        
+        //tamanho da janela
+        setSize(400, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+//=====================================================================================
+
+      //executar o botao limpar
+
+    //   botaoLimpar.addActionListener(new ActionListener(){
+      
+    //     public void actionPerformed(ActionEvent evt){
+    //       act.limparCampos(SetValor, SetSaida);
+    //     }
+    //   });
+
+    // // Códigos de execução Botão Testar
+    // botaoTestar.addActionListener(new ActionListener(){
+    //     public void actionPerformed(ActionEvent evt){
+    //         SetSaida.setText("" + act.testarCampos(SetValor));
+    //     }
+    // });           
+    
+    }
+}

b237eb72e1be664fbce9db5a97d788a0b48f4109
iBabity <luckas.ribeiro2015@gmail.com>
2019-10-14 10:18:57 -0300

    exercicio junit

---
+++ atividade junit (introdução)/src/main/java/model/Usuario.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Usuario{
+
+    private String username;
+    private String rg;
+    private String dataNasc;
+
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRg() {
+        return this.rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getDataNasc() {
+        return this.dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+}

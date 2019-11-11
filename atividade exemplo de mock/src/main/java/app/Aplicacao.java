381352456a24820bbc487207ec56bc398c7efea9
lukasribeiro <luckas.ribeiro2015@gmail.com>
2019-11-11 18:35:37 -0300

    atividade implementação do framework do mock

--- atividade exemplo de mock/src/main/java/app/Aplicacao.java
+++ atividade exemplo de mock/src/main/java/app/Aplicacao.java
@@ -9,11 +9,38 @@ public class Aplicacao{
 
     public static void main(String[] args){
 
-        System.out.println("Efetuando o mock");
+        
+        //  metodo 1
+        System.out.println("Efetuando o mock aplicando o metodo da soma!");
 
         Calculadora calmock = Mockito.mock(Calculadora.class);
         Mockito.when(calmock.somar(10f, 10f)).thenReturn(20f);
         System.out.println(calmock.somar(10f, 10f));   
+    
+
+        //  metodo 2
+        System.out.println("Efetuando o mock efetuando o metodo da subtrção!");
+
+        // Calculadora calmock = Mockito.mock(Calculadora.class);
+        Mockito.when(calmock.subtrair(10f, 10f)).thenReturn(0f);
+        System.out.println(calmock.subtrair(10f, 10f));
+
+
+        // metodo 3
+        
+        System.out.println("Efetuando o mock efetuando o metodo de multiplicação!");
+
+        // Calculadora calmock = Mockito.mock(Calculadora.class);
+        Mockito.when(calmock.multiplicar(10f, 10f)).thenReturn(100f);
+        System.out.println(calmock.multiplicar(10f, 10f));
+
+        
+        // metodo 4 
+        System.out.println("Efetuando o mock efetuando o metodo de multiplicação!");
+
+        // Calculadora calmock = Mockito.mock(Calculadora.class);
+        Mockito.when(calmock.dividir(10f, 10f)).thenReturn(1f);
+        System.out.println(calmock.dividir(10f, 10f));    
     }
 
 }
